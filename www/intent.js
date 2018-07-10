38fe68a252e1ba146cfeafbb2392797a76ae4354
Pulipuli Chen <pulipuli.chen@gmail.com>
2018-07-11 01:02:46 +0800

    0711 忘記修正了

--- www/intent.js
+++ www/intent.js
@@ -3,7 +3,7 @@ var FILTER_SUBJECT = [
 ];
 
 intent_handler = function (intent) {
-    alert("換了 可以嗎？");
+    //alert("換了 可以嗎？");
     //alert(JSON.stringify(intent));
     if (typeof (intent.action) === "string"
             && intent.action === "android.intent.action.MAIN") {
